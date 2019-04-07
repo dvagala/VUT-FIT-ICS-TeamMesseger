11344425d02ae359fa1dca2d615035e666872e25
Ancient <jakub.vins2@gmail.com>
2019-04-07 22:50:22 +0200

    Finishing touches, ready for push to master

--- Entities.test/EntitiesTests.cs
+++ Entities.test/EntitiesTests.cs
@@ -22,7 +22,6 @@ namespace Entities.DAL.test
         public void PostEntityAddTest()
         {
             
-            CommentEntity Comment = new CommentEntity();
             PostEntity PEntity = new PostEntity();
             PEntity.ID = new Guid();
             PEntity.Autor = null;
@@ -81,7 +80,6 @@ namespace Entities.DAL.test
         [Fact]
         public void CommentEntityAddTests()
         {
-            //UserEntity User = new UserEntity(); TODO Zistit ako pridat entitu v entite do databazy
             CommentEntity Comment = new CommentEntity();
             Comment.Autor = null;
             Comment.MessageText = "Ahoj!";
@@ -120,14 +118,12 @@ namespace Entities.DAL.test
         [Fact]
         public void UserEntityAddTest()
         {
-            TeamEntity Team = new TeamEntity();
             UserEntity User = new UserEntity();
             User.Name = "Jojo";
             User.Surname = "G";
             User.LastActivity = new DateTime(2017, 5, 5);
             User.Email = "xjojog@vutbr.cz";
             User.Password = "123jojo";
-            //User.Teams.Add(Team); TODO zistit ako pridavat entitu v entite do databazy
             
 
             //Act
@@ -177,8 +173,7 @@ namespace Entities.DAL.test
         [Fact]
         public void TeamEntityAddTest()
         {
-            UserEntity User = new UserEntity();
-            var PEntity = new PostEntity();
+
             var TEntity = new TeamEntity();
             TEntity.Name = "Bang!";
             TEntity.Description = "You're gonna carry that weight";
@@ -235,77 +230,4 @@ namespace Entities.DAL.test
 
 
     }
-
-    //[Fact]
-    //public void UserEntityTest()
-    //{
-
-    //    var id = new Guid();
-    //    var TestUserEntity = new UserEntity
-    //    {
-    //        ID = id,
-    //        UserName = "Spike",
-    //        LastActivity = new TimeSpan(15, 10, 5),
-    //        Email = new EmailAdressEntity
-    //        {
-    //            ID = new Guid(),
-    //            Email = "Spiegel@gmail.com"
-    //        }
-    //};
-
-    //    using (var db = new DataDBContext()) 
-    //    {
-    //        db.Users.Add(TestUserEntity);
-    //        db.SaveChanges();
-    //    }
-
-    //    using (var db = new DataDBContext())
-    //    {
-    //        var User = db.Users.FirstOrDefault();
-    //        Assert.Equal(TestUserEntity,User);
-
-
-    //    }
-
-    //}
-
-
-    //}
-    //[Fact]
-    //public void CommentEntityTests()
-    //{
-    //    UserEntity User = new UserEntity();
-    //    CommentEntity Comment = new CommentEntity();
-    //    Comment.Autor = User;
-    //    Comment.MessageText = "Ahoj!";
-    //    Comment.PublishDate = new TimeSpan(15, 49, 30);
-    //}
-    //[Fact]
-    //public void PostEntityTest()
-    //{
-    //    CommentEntity Comment = new CommentEntity();
-    //    PostEntity PEntity = new PostEntity();
-    //    PEntity.ID = new Guid();
-    //    PEntity.MessageText = "See you space cowboy!";
-    //    PEntity.PublishDate = new TimeSpan(10, 10, 10);
-    //    PEntity.Title = "Accelerated spread of Weebs";
-    //    PEntity.Comments.Add(Comment);
-    //}
-    //[Fact]
-    //public void TeamEntityTest()
-    //{
-    //    UserEntity User = new UserEntity();
-    //    var PEntity = new PostEntity();
-    //    var TEntity = new TeamEntity();
-    //    TEntity.Name = "Bang!";
-    //    TEntity.Description = "You're gonna carry that weight";
-    //    TEntity.Users.Add(User);
-    //    TEntity.Posts.Add(PEntity);
-    //}
-
-    //[Fact]
-
-
-    //MessemgerDbContext
-
 }
