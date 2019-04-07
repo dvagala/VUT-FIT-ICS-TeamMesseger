4e5010bb2df9752ae844f94e317540a4821eb379
Kristián Glós <xglosk00@vutbr.cz>
2019-04-07 22:15:03 +0000

    Revert "Revert "Merged PR 2: Phase2
    Phase 2 of the team communication app - Connected with DB.
    
    Related work items: #10, #14, #15, #16, #17, #18, #19""

---
+++ ICS.Project.BL/Repositories/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using ICS.Project.BL.Models;
+
+namespace ICS.Project.BL.Repositories
+{
+    public interface IUsersRepository
+    {
+        IEnumerable<UserModel> GetAll();
+        UserModel GetById(Guid id);
+        void Update(UserModel post);
+        UserModel Add(UserModel post);
+        void Remove(Guid id);
+    }
+}
