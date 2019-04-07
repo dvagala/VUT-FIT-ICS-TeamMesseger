09375d63bad5dc3a6b1bffb87a0ea83e4e195dda
Zhamilya Abikenova <xabike00@vutbr.cz>
2019-04-07 14:02:57 +0200

    Add MessengerDbContextTestsClassSetupFixture.cs implemented DAL.Tests, using in memory DB

--- Entities.test/EntitiesTests.cs
+++ Entities.test/EntitiesTests.cs
@@ -1,5 +1,6 @@
 ﻿using ICS.Project.DAL;
 using ICS.Project.DAL.Entities;
+using MessengerDbContext.DAL.Tests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,101 +8,190 @@ using Xunit;
 
 namespace Entities.DAL.test
 {
-    public class EntitiesTests {
+    public class EntitiesTests : IClassFixture<MessengerDbContextTestsClassSetupFixture>
+    {
+        public EntitiesTests(MessengerDbContextTestsClassSetupFixture testContext)
+        {
+            _testContext = testContext;
+        }
+
+        private readonly MessengerDbContextTestsClassSetupFixture _testContext;
 
         [Fact]
-        public void EmailAdressEntityTest()
+        public void PostEntityAddTest()
         {
-            //var id = new Guid();
-            //var TestEmailAdressEntity = new EmailAdressEntity
-            //{
-            //    ID = id,
-            //    Email = "Spike@gmail.com"
-            //};
-            //using (var db = new DataDBContext())
-            //{
-            //    db.Emails.Add(TestEmailAdressEntity);
-            //    db.SaveChanges();
-            //}
-            ////TODO Treba opravit, Addnuty email neostava v databaze
-            //using (var db = new DataDBContext())
-            //{
-            //    var Email = db.Emails.FirstOrDefault(x => x.ID == id);
-            //    Assert.Equal(TestEmailAdressEntity, Email);
-            //}
+            
+            CommentEntity Comment = new CommentEntity();
+            PostEntity PEntity = new PostEntity();
+            PEntity.ID = new Guid();
+            PEntity.MessageText = "See you space cowboy!";
+            PEntity.PublishDate = new DateTime(2018, 10, 10);
+            PEntity.Title = "Accelerated spread of Weebs";
+            PEntity.Comments.Add(Comment);
+
+            //Act
+            _testContext.MessengerDbContextSUT.Posts.Add(PEntity);
+            _testContext.MessengerDbContextSUT.SaveChanges();
+
+            //Assert
+            using (var dbx = _testContext.CreateMessengerDbContext())
+            {
+                var retievedPost = dbx.Posts.First(entity => entity.ID == PEntity.ID);
+                Assert.Equal(PEntity, retievedPost);
+            }
         }
 
         //[Fact]
-        //public void UserEntityTest()
+        //public void PostEntityDeleteTest()
         //{
-       
-        //    var id = new Guid();
-        //    var TestUserEntity = new UserEntity
-        //    {
-        //        ID = id,
-        //        UserName = "Spike",
-        //        LastActivity = new TimeSpan(15, 10, 5),
-        //        Email = new EmailAdressEntity
-        //        {
-        //            ID = new Guid(),
-        //            Email = "Spiegel@gmail.com"
-        //        }
+          //  using (var context = new MessengerDbContext()) ;
+          //PostEntity PEntity = new PostEntity() { Id = 1};
+          //  context.PEntity.Attach(PEntity);
+          //  context.PEntity.Remove(PEntity);
+          //  context.SaveChanges();
+      //  }
+        //[Fact]
+        //public void CommentEntityAddTests()
+        //{
+            //UserEntity User = new UserEntity();
+            //CommentEntity Comment = new CommentEntity();
+            //Comment.Autor = User;
+            //Comment.MessageText = "Ahoj!";
+            //Comment.PublishDate = new TimeSpan(15, 49, 30);
+  
+        //}
+        /*
+        [Fact]
+
+        public void UserEntityAddTest()
+        {
+            TeamEntity Team = new TeamEntity();
+            UserEntity User = new UserEntity();
+            User.Name = "Jojo";
+            User.Surname = "G";
+            User.LastActivity = new TimeSpan(15, 49, 30);
+            User.Email = "xjojog@vutbr.cz";
+            User.Password = "123jojo";
+            User.Teams.Add(Team);
+
+            using (var db = new MessengerDbContext()) ;
+            db.add(User);
+            db.saveChanges();
+        }
+
+
+        public void TeamEntityAddTest()
+        {
+            UserEntity User = new UserEntity();
+            var PEntity = new PostEntity();
+            var TEntity = new TeamEntity();
+            TEntity.Name = "Bang!";
+            TEntity.Description = "You're gonna carry that weight"; 
+            TEntity.Users.Add(User);
+            TEntity.Posts.Add(PEntity);
+
+
+            using (var db = new MessengerDbContext()) ;
+            db.add(TEntity);
+            db.saveChanges();
+        }
+
+
+        public void TeamEntityDeleteTest()
+        {
+
+
+
+        }
+        */
+        //[Fact]
+        //{
+        //    ID = id,
+        //    Email = "Spike@gmail.com"
         //};
-
-        //    using (var db = new DataDBContext()) 
-        //    {
-        //        db.Users.Add(TestUserEntity);
-        //        db.SaveChanges();
-        //    }
-
-        //    using (var db = new DataDBContext())
-        //    {
-        //        var User = db.Users.FirstOrDefault();
-        //        Assert.Equal(TestUserEntity,User);
-                
-
-        //    }
-
-        //}
-            
-            
-        //}
-        //[Fact]
-        //public void CommentEntityTests()
+        //using (var db = new DataDBContext())
         //{
-        //    UserEntity User = new UserEntity();
-        //    CommentEntity Comment = new CommentEntity();
-        //    Comment.Autor = User;
-        //    Comment.MessageText = "Ahoj!";
-        //    Comment.PublishDate = new TimeSpan(15, 49, 30);
+        //    db.Emails.Add(TestEmailAdressEntity);
+        //    db.SaveChanges();
         //}
-        //[Fact]
-        //public void PostEntityTest()
+        ////TODO Treba opravit, Addnuty email neostava v databaze
+        //using (var db = new DataDBContext())
         //{
-        //    CommentEntity Comment = new CommentEntity();
-        //    PostEntity PEntity = new PostEntity();
-        //    PEntity.ID = new Guid();
-        //    PEntity.MessageText = "See you space cowboy!";
-        //    PEntity.PublishDate = new TimeSpan(10, 10, 10);
-        //    PEntity.Title = "Accelerated spread of Weebs";
-        //    PEntity.Comments.Add(Comment);
+        //    var Email = db.Emails.FirstOrDefault(x => x.ID == id);
+        //    Assert.Equal(TestEmailAdressEntity, Email);
         //}
-        //[Fact]
-        //public void TeamEntityTest()
-        //{
-        //    UserEntity User = new UserEntity();
-        //    var PEntity = new PostEntity();
-        //    var TEntity = new TeamEntity();
-        //    TEntity.Name = "Bang!";
-        //    TEntity.Description = "You're gonna carry that weight";
-        //    TEntity.Users.Add(User);
-        //    TEntity.Posts.Add(PEntity);
-        //}
-        
-        
-        
-        
-
-        
     }
+
+    //[Fact]
+    //public void UserEntityTest()
+    //{
+
+    //    var id = new Guid();
+    //    var TestUserEntity = new UserEntity
+    //    {
+    //        ID = id,
+    //        UserName = "Spike",
+    //        LastActivity = new TimeSpan(15, 10, 5),
+    //        Email = new EmailAdressEntity
+    //        {
+    //            ID = new Guid(),
+    //            Email = "Spiegel@gmail.com"
+    //        }
+    //};
+
+    //    using (var db = new DataDBContext()) 
+    //    {
+    //        db.Users.Add(TestUserEntity);
+    //        db.SaveChanges();
+    //    }
+
+    //    using (var db = new DataDBContext())
+    //    {
+    //        var User = db.Users.FirstOrDefault();
+    //        Assert.Equal(TestUserEntity,User);
+
+
+    //    }
+
+    //}
+
+
+    //}
+    //[Fact]
+    //public void CommentEntityTests()
+    //{
+    //    UserEntity User = new UserEntity();
+    //    CommentEntity Comment = new CommentEntity();
+    //    Comment.Autor = User;
+    //    Comment.MessageText = "Ahoj!";
+    //    Comment.PublishDate = new TimeSpan(15, 49, 30);
+    //}
+    //[Fact]
+    //public void PostEntityTest()
+    //{
+    //    CommentEntity Comment = new CommentEntity();
+    //    PostEntity PEntity = new PostEntity();
+    //    PEntity.ID = new Guid();
+    //    PEntity.MessageText = "See you space cowboy!";
+    //    PEntity.PublishDate = new TimeSpan(10, 10, 10);
+    //    PEntity.Title = "Accelerated spread of Weebs";
+    //    PEntity.Comments.Add(Comment);
+    //}
+    //[Fact]
+    //public void TeamEntityTest()
+    //{
+    //    UserEntity User = new UserEntity();
+    //    var PEntity = new PostEntity();
+    //    var TEntity = new TeamEntity();
+    //    TEntity.Name = "Bang!";
+    //    TEntity.Description = "You're gonna carry that weight";
+    //    TEntity.Users.Add(User);
+    //    TEntity.Posts.Add(PEntity);
+    //}
+
+    //[Fact]
+
+
+    //MessemgerDbContext
+
 }
---
+++ Entities.test/MessengerDbContextTestsClassSetupFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace MessengerDbContext.DAL.Tests
+{
+    public class MessengerDbContextTestsClassSetupFixture : IDisposable
+    {
+        public MessengerDbContext MessengerDbContextSUT { get; set; }
+
+        public MessengerDbContextTestsClassSetupFixture()
+        {
+            this.MessengerDbContextSUT = CreateMessengerDbContext();
+        }
+
+        public MessengerDbContext CreateMessengerDbContext()
+        {
+            return new MessengerDbContext(CreateDbContextOptions());
+        }
+
+        private DbContextOptions<MessengerDbContext> CreateDbContextOptions()
+        {
+            var contextOptionsBuilder = new DbContextOptionsBuilder<MessengerDbContext>();
+            contextOptionsBuilder.UseInMemoryDatabase("Messenger");
+            return contextOptionsBuilder.Options;
+        }
+
+        public void Dispose()
+        {
+            MessengerDbContextSUT?.Dispose();
+        }
+    }
+}
