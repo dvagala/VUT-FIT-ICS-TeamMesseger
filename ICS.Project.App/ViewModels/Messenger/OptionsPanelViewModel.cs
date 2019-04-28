b70907a6bdfca73453763c7c90e4409655728eca
Dominik Vagala <d.vagala@gmail.com>
2019-04-28 19:17:30 +0200

    Rollback Fody so pipeline would build

---
+++ ICS.Project.App/ViewModels/Messenger/OptionsPanelViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using ICS.Project.App.Commands;
+using ICS.Project.BL.Messages;
+using ICS.Project.BL.Models;
+using ICS.Project.BL.Repositories;
+using ICS.Project.BL.Services;
+
+namespace ICS.Project.App.ViewModels.Messenger
+{
+    public class OptionsPanelViewModel : ViewModelBase, IViewModel
+    {
+        private readonly IUsersRepository _usersRepository;
+        private readonly IMediator _mediator;
+
+        public ICommand ChangeEmailCommand { get; set; }
+        public ICommand ChangePasswordCommand { get; set; }
+        public ICommand LogoutCommand { get; set; }
+        public ICommand DeleteAccountCommand { get; set; }
+        public ICommand RefreshCommand { get; set; }
+
+        public UserModel User { get; set; }
+        public string PlainTextPassword { get; set; }
+
+
+        public OptionsPanelViewModel(IUsersRepository usersRepository, IMediator mediator)
+        {
+            ChangeEmailCommand = new RelayCommand(ChangeEmail);
+            ChangePasswordCommand = new RelayCommand(ChangePassword);
+            DeleteAccountCommand = new RelayCommand(DeleteAccount);
+            LogoutCommand = new RelayCommand(Logout);
+            RefreshCommand = new RelayCommand(Refresh);
+
+            _usersRepository = usersRepository;
+            _mediator = mediator;
+        }
+
+        public void Load()
+        {
+            PlainTextPassword = "";
+            User = new UserModel();
+        }
+
+        public void ChangePassword()
+        {
+
+        }
+
+        public void ChangeEmail()
+        {
+            MessageBox.Show("changemeil");
+        }
+
+        public void DeleteAccount()
+        {
+
+        }
+
+        private void Logout()
+        {
+            _mediator.Send(new GoToLoginScreenMessage());
+        }
+
+        private void Refresh()
+        {
+        }
+    }
+}
