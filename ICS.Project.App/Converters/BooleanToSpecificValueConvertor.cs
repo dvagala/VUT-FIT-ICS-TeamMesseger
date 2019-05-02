e97eb389d620734cdc84f025914220746baedea2
Dominik Vagala <d.vagala@gmail.com>
2019-05-02 18:27:18 +0200

    Add collapsing button to team panel

---
+++ ICS.Project.App/Converters/BooleanToSpecificValueConvertor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ICS.Project.App.Converters
+{
+    class BooleanToSpecificValueConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue && parameter is string stringParameter && int.TryParse(stringParameter, out var intParameter))
+            {
+                return boolValue ? intParameter : 0;
+            }
+            else
+            {
+                throw new ArgumentException();               
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
